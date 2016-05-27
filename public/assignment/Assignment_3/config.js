5a58286e39d2aaa33d5d8dbe70d212268dafe228
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2016-05-27 13:01:50 -0400

    Page New View finished   -Same bug is persisting

--- public/assignment/Assignment_3/config.js
+++ public/assignment/Assignment_3/config.js
@@ -51,7 +51,9 @@
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/new", {
-                templateUrl: "views/page/page-new.view.client.html"
+                templateUrl: "views/page/page-new.view.client.html",
+                controller: "NewPageController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid", {
                 templateUrl: "views/page/page-edit.view.client.html",
