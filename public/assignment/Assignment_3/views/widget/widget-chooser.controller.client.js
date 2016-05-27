246818a72bf533ba36a7bc37d77a89fdb99cead1
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2016-05-27 15:08:14 -0400

    Widget Choose completed

--- public/assignment/Assignment_3/views/widget/widget-chooser.controller.client.js
+++ public/assignment/Assignment_3/views/widget/widget-chooser.controller.client.js
@@ -3,8 +3,11 @@
         .module("WebAppMaker")
         .controller("WidgetChooserController", WidgetChooserController);
 
-    function WidgetChooserController($routeParams, WidgetService) {
+    function WidgetChooserController($routeParams, WidgetService, $location) {
         var vm = this;
+        vm.newHeader = newHeader;
+        vm.newYoutube = newYoutube;
+        vm.newImage = newImage;
 
         function init() {
             vm.uid = $routeParams["uid"];
@@ -12,5 +15,33 @@
             vm.pid = $routeParams["pid"];
         }
         init();
+
+        function newHeader(){
+            console.log("New Header!");
+            var header = {};
+            header._id = (Math.floor(Math.random()*90000) + 10000).toString();
+            header.widgetType = "HEADER";
+            header.pageId = vm.pid;
+            WidgetService.createWidget(header._id, header);
+            $location.url("/user/" + vm.uid + "/website/" + vm.wid + "/page/" + vm.pid + "/widget/" + header._id);
+        }
+        function newYoutube(){
+            console.log("New Youtube Link!");
+            var header = {};
+            header._id = (Math.floor(Math.random()*90000) + 10000).toString();
+            header.widgetType = "YOUTUBE";
+            header.pageId = vm.pid;
+            WidgetService.createWidget(header._id, header);
+            $location.url("/user/" + vm.uid + "/website/" + vm.wid + "/page/" + vm.pid + "/widget/" + header._id);
+        }
+        function newImage(){
+            console.log("New Image!");
+            var header = {};
+            header._id = (Math.floor(Math.random()*90000) + 10000).toString();
+            header.widgetType = "IMAGE";
+            header.pageId = vm.pid;
+            WidgetService.createWidget(header._id, header);
+            $location.url("/user/" + vm.uid + "/website/" + vm.wid + "/page/" + vm.pid + "/widget/" + header._id);
+        }
     }
 })();
