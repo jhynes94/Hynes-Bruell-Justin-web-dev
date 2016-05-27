8a4fdda9272494ef8c144221cd85f7c45036b56a
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2016-05-27 01:41:00 -0400

    Register is now Finished!

--- public/assignment/Assignment_3/views/user/login.controller.client.js
+++ public/assignment/Assignment_3/views/user/login.controller.client.js
@@ -8,6 +8,7 @@
         var vm = this;
 
         vm.login = login;
+        vm.createNewUser = createNewUser;
 
         function login (username, password) {
             var user = UserService.findUserByUsernameAndPassword(username, password);
@@ -17,6 +18,18 @@
             } else {
                 vm.error = "User not found";
             }
+        }
+
+
+        //TODO add randomly created ID
+        function createNewUser(username, password, vPassword){
+            if(!(password === vPassword)){
+                vm.error = "Non-Matching Passwords";
+                return null;
+            }
+            var newUser = {_id: "444", username: username, password: password}
+            UserService.createUser(newUser);
+            $location.url("/user/" + newUser._id);
         }
     }
 })();
