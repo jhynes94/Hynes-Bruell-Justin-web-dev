605acdd76f972c2cc7bc32dfc63afd315a61bc60
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2016-06-05 18:13:09 -0400

    Code Cleanup

--- public/assignment/Assignment_4/services/page.service.client.js
+++ public/assignment/Assignment_4/services/page.service.client.js
@@ -1,7 +1,7 @@
-(function() {
+(function () {
     angular.module("WebAppMaker")
         .factory("PageService", PageService);
-    
+
     function PageService($http) {
 
         var api = {
@@ -14,28 +14,27 @@
         };
         return api;
 
-        //TODO
+        
         function createPage(websiteId, page) {
             var url = "/api/website/" + websiteId + "/page/";
             return $http.post(url, page);
         }
-        //TODO
+
         function findPageByWebsiteId(websiteId) {
             var url = "/api/website/" + websiteId + "/page/";
             return $http.get(url);
         }
 
-        //TODO
         function findPageById(pageId) {
             var url = "/api/page/" + pageId;
             return $http.get(url);
         }
-        //TODO
+
         function updatePage(pageId, page) {
             var url = "/api/page/" + pageId;
             return $http.put(url, page);
         }
-        //TODO
+
         function deletePage(pageId) {
             var url = "/api/page/" + pageId;
             return $http.delete(url);
