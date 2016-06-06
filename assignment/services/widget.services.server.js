237dd0f352878ff41743d4cdca15767181504160
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2016-06-05 20:09:01 -0400

    Upload image Now working

--- assignment/services/widget.services.server.js
+++ assignment/services/widget.services.server.js
@@ -1,5 +1,8 @@
 module.exports = function (app) {
     
+    var multer = require('multer'); // npm install multer --save
+    var upload = multer({ dest: __dirname+'/../../public/uploads' });
+    
     var widgets = [
         { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
         { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
@@ -17,6 +20,7 @@ module.exports = function (app) {
     app.get("/api/widget/:widgetId", findWidgetById);
     app.put("/api/widget/:widgetId", updateWidget);
     app.delete("/api/widget/:widgetId", deleteWidget);
+    app.post ("/api/upload", upload.single('myFile'), uploadImage);
 
 
     function createWidget(req, res) {
@@ -73,5 +77,19 @@ module.exports = function (app) {
         }
         res.status(404).send("Unable to remove widget with ID: " + id);
     }
+
+    function uploadImage(req, res) {
+
+        var widgetId      = req.body.widgetId;
+        var width         = req.body.width;
+        var myFile        = req.file;
+
+        var originalname  = myFile.originalname; // file name on user's computer
+        var filename      = myFile.filename;     // new file name in upload folder
+        var path          = myFile.path;         // full path of uploaded file
+        var destination   = myFile.destination;  // folder where file is saved to
+        var size          = myFile.size;
+        var mimetype      = myFile.mimetype;
+    }
     
-}
+};
